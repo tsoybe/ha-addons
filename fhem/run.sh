37a625de743f127cb5dca9f66ff74dba56e73d1d
tsyobe <51762970+tsoybe@users.noreply.github.com>
2025-05-09 22:28:23 +0200

    eine Version

---
+++ fhem/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+echo "##############################"
+
+if [ ! -f "/data/fhem.cfg" ]; then
+   cat <<EOF
+attr global logfile -
+attr global modpath .
+attr global verbose 3
+attr global statefile /data/fhem.save
+attr global room System
+define WEB FHEMWEB 8083 global
+attr WEB room System
+define eventTypes eventTypes ./log/eventTypes.txt
+attr eventTypes room System
+EOF > /data/fhem.cfg
+    echo "Standardkonfig erstellt."
+else
+    echo "Konfig gefunden:"
+    echo "################"
+    cat /data/fhem.cfg
+    echo "################"
+fi
+
+cd /opt/fhem
+exec /usr/bin/perl fhem.pl /data/fhem.cfg
+echo "############# END #############"
