b56d9303c3ed11f27dcbf296ae498c60740be53f
tsyobe <51762970+tsoybe@users.noreply.github.com>
2025-05-15 00:28:34 +0200

    d

--- miniflux/run.sh
+++ miniflux/run.sh
@@ -7,9 +7,22 @@ DB_PORT=$(jq -r '.db_port' /data/options.json)
 DB_NAME=$(jq -r '.db_name' /data/options.json)
 DB_USER=$(jq -r '.db_user' /data/options.json)
 DB_PASSWORD=$(jq -r '.db_password' /data/options.json)
+CREATE_ADMIN=$(jq -r '.create_admin' /data/options.json)
+
+if [ "$CREATE_ADMIN" = "true" ]; then
+  export CREATE_ADMIN=1
+  export ADMIN_USERNAME=$(jq -r '.admin_username' /data/options.json)
+  export ADMIN_PASSWORD=$(jq -r '.admin_password' /data/options.json)
+  echo "Create Admin: $ADMIN_USERNAME"
+else
+  export CREATE_ADMIN=0
+fi
 
 echo "DATABASE_URL=\"postgres://${DB_USER}:***@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable\""
 
 export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
+export RUN_MIGRATIONS=1
 
+
+echo "##############################"
 exec /usr/bin/miniflux
