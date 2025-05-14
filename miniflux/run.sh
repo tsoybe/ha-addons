ed3a810ea88b953ddbe17c4118cddee29da9807a
tsyobe <51762970+tsoybe@users.noreply.github.com>
2025-05-14 23:33:49 +0200

    next

--- miniflux/run.sh
+++ miniflux/run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
+
 echo "##############################"
-echo $DATABASE_URL
+
+DB_HOST=$(jq -r '.db_host' /data/options.json)
+DB_PORT=$(jq -r '.db_port' /data/options.json)
+DB_NAME=$(jq -r '.db_name' /data/options.json)
+DB_USER=$(jq -r '.db_user' /data/options.json)
+DB_PASSWORD=$(jq -r '.db_password' /data/options.json)
+
+export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
+
+env
 
 exec /usr/bin/miniflux
