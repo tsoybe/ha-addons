5fe9bb1e555e819abdf4beedf45b70647a00a1f3
tsyobe <51762970+tsoybe@users.noreply.github.com>
2025-05-14 23:55:56 +0200

    b

--- miniflux/run.sh
+++ miniflux/run.sh
@@ -2,16 +2,14 @@
 
 echo "##############################"
 
-cat /data/options.json
-
 DB_HOST=$(jq -r '.db_host' /data/options.json)
 DB_PORT=$(jq -r '.db_port' /data/options.json)
 DB_NAME=$(jq -r '.db_name' /data/options.json)
 DB_USER=$(jq -r '.db_user' /data/options.json)
 DB_PASSWORD=$(jq -r '.db_password' /data/options.json)
 
+echo "DATABASE_URL=\"postgres://${DB_USER}:***@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable\""
+
 export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
 
-env
-
-exec su-exec nobody /usr/bin/miniflux
+exec /usr/bin/miniflux
