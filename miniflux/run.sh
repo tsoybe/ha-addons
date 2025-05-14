ccbc2538584a23cbe4e20d1359e01af08ee56ff0
tsyobe <51762970+tsoybe@users.noreply.github.com>
2025-05-14 23:24:36 +0200

    debug

--- miniflux/run.sh
+++ miniflux/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
+echo "##############################"
+echo $DATABASE_URL
+
 exec /usr/bin/miniflux
