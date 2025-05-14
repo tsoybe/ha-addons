b6382ef9e023356d2f6f1df6d9803cffc16f19ce
tsyobe <51762970+tsoybe@users.noreply.github.com>
2025-05-15 01:22:53 +0200

    dd

--- miniflux/run.sh
+++ miniflux/run.sh
@@ -2,13 +2,18 @@
 
 echo "##############################"
 
+export BASE_URL=$(jq -r '.base_url' /data/options.json)
+echo "BaseURL: $BASE_URL"
+
+export RUN_MIGRATIONS=1
+
 DB_HOST=$(jq -r '.db_host' /data/options.json)
 DB_PORT=$(jq -r '.db_port' /data/options.json)
 DB_NAME=$(jq -r '.db_name' /data/options.json)
 DB_USER=$(jq -r '.db_user' /data/options.json)
 DB_PASSWORD=$(jq -r '.db_password' /data/options.json)
-
-BASE_URL=$(jq -r '.base_url' /data/options.json)
+export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
+echo "DATABASE_URL=\"postgres://${DB_USER}:***@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable\""
 
 CREATE_ADMIN=$(jq -r '.create_admin' /data/options.json)
 if [ "$CREATE_ADMIN" = "true" ]; then
@@ -19,12 +24,6 @@ if [ "$CREATE_ADMIN" = "true" ]; then
 else
   export CREATE_ADMIN=0
 fi
-
-echo "DATABASE_URL=\"postgres://${DB_USER}:***@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable\""
-
-export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
-export RUN_MIGRATIONS=1
-
 
 echo "##############################"
 exec /usr/bin/miniflux
